5131e3bc15f53709fb0c780a972d32f187dc45f8
vijayadithya330 <88371471+vijayadithya330@users.noreply.github.com>
2021-09-23 07:18:20 +0530

    Update nopm.py

--- plugins/nopm.py
+++ plugins/nopm.py
@@ -42,7 +42,7 @@ async def nopm(client, message):
             )
         except BotInlineDisabled:
             print(f"[WARN] - Inline Mode for @{USERNAME} is not enabled. Enable from @Botfather to enable PM Permit !")
-            await message.reply_text(f"{REPLY_MESSAGE}\n\n<b>© Powered By : \n@AsmSafone | @SafoTheBot 👑</b>")
+            await message.reply_text(f"{REPLY_MESSAGE}\n\n<b>🧑🏻‍💻 Powered By:</b> @VijayAdithyaa")
         except Exception as e:
             print(e)
             pass
